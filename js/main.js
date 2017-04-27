53e659e2911fb1ed5c0c5d8392c5400211b421d5
sw <pgcode84@gmail.com>
2017-04-27 12:25:01 +0800

    change bg-color, fix form validation

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+sendButton.onclick = function () {
+	console.log("TTTTTTTTSTSTS")
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
+
+
+//<a onclick="javascript: href="slowpage.html">Slow Page</a>
