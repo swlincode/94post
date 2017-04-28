9c272a72828b4258929e5f70391edb97eab88c40
sw <pgcode84@gmail.com>
2017-04-28 18:45:27 +0800

    add 2 container, auto progress bar animation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,51 @@
+//demoButton.onclick = function() {
+//		document.getElementById('progress_1')
+//			.style.width = (Math.random() * 35) + 1 + "%";
+//		document.getElementById('progress_2')
+//			.style.width = (Math.random() * 50) + 1 + "%";
+//		document.getElementById('progress_3')
+//			.style.width = (Math.random() * 90) + 1 + "%";
+//		document.getElementById('progress_4')
+//			.style.width = (Math.random() * 80) + 1 + "%";
+//
+//}
 
+//function delayButton() {
+//		setTimeout(function() {
+//			var progress1_Num = ((Math.random() * 35) + 1);
+//			var progress2_Num = ((Math.random() * 35) + 1);
+//			var progress3_Num = ((Math.random() * 35) + 1);
+//			var progress4_Num = ((Math.random() * 35) + 1);
+//	
+//			document.getElementById('progress_1').style.width = progress1_Num + "%";
+//			document.getElementById('progress_2').style.width = progress2_Num + "%";
+//			document.getElementById('progress_3').style.width = progress3_Num + "%";
+//			document.getElementById('progress_4').style.width = progress4_Num + "%";
+//			document.getElementById('progress_1_text').innerHTML = Math.floor(progress1_Num) + "篇";
+//			document.getElementById('progress_2_text').innerHTML = Math.floor(progress2_Num) + "篇";
+//			document.getElementById('progress_3_text').innerHTML = Math.floor(progress3_Num) + "篇";
+//			document.getElementById('progress_4_text').innerHTML = Math.floor(progress4_Num) + "篇";
+//			
+//			}, 1000);
+// }
 
-sendButton.onclick = function () {
-	console.log("TTTTTTTTSTSTS")
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
+setInterval(function() {
+			var progress1_Num = ((Math.random() * 35) + 1);
+			var progress2_Num = ((Math.random() * 35) + 1);
+			var progress3_Num = ((Math.random() * 35) + 1);
+			var progress4_Num = ((Math.random() * 35) + 1);
+			var summation = Math.floor(progress1_Num+progress1_Num+progress1_Num+progress1_Num);
+	
+			document.getElementById('progress_1').style.width = progress1_Num + "%";
+			document.getElementById('progress_2').style.width = progress2_Num + "%";
+			document.getElementById('progress_3').style.width = progress3_Num + "%";
+			document.getElementById('progress_4').style.width = progress4_Num + "%";
+			document.getElementById('progress_1_text').innerHTML = Math.floor(progress1_Num) + "篇";
+			document.getElementById('progress_2_text').innerHTML = Math.floor(progress2_Num) + "篇";
+			document.getElementById('progress_3_text').innerHTML = Math.floor(progress3_Num) + "篇";
+			document.getElementById('progress_4_text').innerHTML = Math.floor(progress4_Num) + "篇";
+				document.getElementById('autoSum').innerHTML = summation;
+	
+			
+			}, 3000);
 
-
-
-//<a onclick="javascript: href="slowpage.html">Slow Page</a>
