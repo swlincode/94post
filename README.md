91088134e18ee5a3ea37027cd8abb1c297429a4f
sw <pgcode84@gmail.com>
2017-04-30 23:06:06 +0800

    organize folder

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# r-d
+![Markdown](http://markdown.tw/images/208x128.png)
+ * * *
+###Bootstrap.css
+ * [Jumbotron][1]
+ * [Glyphicons][2]
+ * [Progress Bars][3]
+   
+  [1]: https://www.w3schools.com/bootstrap/bootstrap_jumbotron_header.asp
+  [2]: http://getbootstrap.com/components/
+  [3]: https://www.w3schools.com/bootstrap/bootstrap_progressbars.asp
+
+###Google Fonts
+   - [Montserrat, sans-serif][1]
+   - [Concert One, cursive][2]
+   - [Lato, sans-serif][3]
+   
+  [1]: https://fonts.google.com/specimen/Montserrat
+  [2]: https://fonts.google.com/specimen/Concert+One
+  [3]: https://fonts.google.com/specimen/Lato
+  * * *
+###心得
+####1.引用程式碼要從頭到尾檢查
+
+![enter image description here](dde.com/dsa.jpg)
+
+####2.善用Chrome Console
+######Console會告訴你錯在哪一行，大部分錯誤都能在Error message裡找到，很多時候只是拼錯字....
+![enter image description here](dde.com/dsa.jpg)
+####3. Brackets Live Preview是好幫手
+######Live Preview會提示你目前網頁點選的區塊，並且告訴你是哪一行Code
+![enter image description here](dde.com/dsa.jpg)
--- js/main.js
+++ js/main.js
@@ -1,34 +1,3 @@
-//demoButton.onclick = function() {
-//		document.getElementById('progress_1')
-//			.style.width = (Math.random() * 35) + 1 + "%";
-//		document.getElementById('progress_2')
-//			.style.width = (Math.random() * 50) + 1 + "%";
-//		document.getElementById('progress_3')
-//			.style.width = (Math.random() * 90) + 1 + "%";
-//		document.getElementById('progress_4')
-//			.style.width = (Math.random() * 80) + 1 + "%";
-//
-//}
-
-//function delayButton() {
-//		setTimeout(function() {
-//			var progress1_Num = ((Math.random() * 35) + 1);
-//			var progress2_Num = ((Math.random() * 35) + 1);
-//			var progress3_Num = ((Math.random() * 35) + 1);
-//			var progress4_Num = ((Math.random() * 35) + 1);
-//	
-//			document.getElementById('progress_1').style.width = progress1_Num + "%";
-//			document.getElementById('progress_2').style.width = progress2_Num + "%";
-//			document.getElementById('progress_3').style.width = progress3_Num + "%";
-//			document.getElementById('progress_4').style.width = progress4_Num + "%";
-//			document.getElementById('progress_1_text').innerHTML = Math.floor(progress1_Num) + "篇";
-//			document.getElementById('progress_2_text').innerHTML = Math.floor(progress2_Num) + "篇";
-//			document.getElementById('progress_3_text').innerHTML = Math.floor(progress3_Num) + "篇";
-//			document.getElementById('progress_4_text').innerHTML = Math.floor(progress4_Num) + "篇";
-//			
-//			}, 1000);
-// }
-
 setInterval(function() {
 			var progress1_Num = ((Math.random() * 35) + 1);
 			var progress2_Num = ((Math.random() * 35) + 1);
@@ -45,6 +14,8 @@ setInterval(function() {
 			document.getElementById('progress_3_text').innerHTML = Math.floor(progress3_Num) + "篇";
 			document.getElementById('progress_4_text').innerHTML = Math.floor(progress4_Num) + "篇";
 				document.getElementById('autoSum').innerHTML = summation;
+        document.getElementById('p4_text_num').innerHTML = Math.floor(progress4_Num);
+    
 	
 			
 			}, 3000);
